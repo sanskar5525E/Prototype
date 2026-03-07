60d3510581a93c1fb3fa633fdae4f5713df8a0cd
Sanskarjyotibehera <sanskarjyoti45@gmail.com>
2026-03-07 19:18:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Simple Sales Dashboard")
+
+# upload excel
+file = st.file_uploader("Upload Excel file", type=["xlsx","csv"])
+
+if file:
+    df = pd.read_excel(file)
+
+    st.subheader("Data Preview")
+    st.dataframe(df)
+
+    total_sales = df["Amount"].sum()
+
+    st.metric("Total Sales", total_sales)
