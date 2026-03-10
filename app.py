0e4b231599d1d4a0c6a0b7911cbc3044f1dbc13e
Sanskarjyotibehera <sanskarjyoti45@gmail.com>
2026-03-10 15:26:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,1 @@
-import streamlit as st
-import pandas as pd
 
-st.title("Simple Sales Dashboard")
-
-# upload excel
-file = st.file_uploader("Upload Excel file", type=["xlsx","csv"])
-
-if file:
-    df = pd.read_csv(file)
-
-    st.subheader("Data Preview")
-    st.dataframe(df)
-
-    total_sales = df["Amount"].sum()
-
-    st.metric("Total Sales", total_sales)
